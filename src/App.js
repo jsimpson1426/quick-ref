f8f515e4d2cb7966f13c932013f02f8fdbd47c52
Simpson <joshuasimpson3@usicllc.com>
2020-03-04 11:28:36 -0500

    Refactored App.js; Created CardCollection

--- src/App.js
+++ src/App.js
@@ -1,81 +1,69 @@
 import React, { Component } from "react";
 import NavBar from "./components/common/navBar/navBar";
-import CardDeck from "react-bootstrap/CardDeck";
-import MyCard from "./components/common/myCard/myCard";
+import CardCollection from "./components/common/cardCollection/cardCollection";
 import "./App.sass";
 
 class App extends Component {
   state = {
     cards: [
-      [
-        {
-          title: "Title One",
-          description:
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
-          buttonLink: "#",
-          buttonText: "View Material"
-        },
-        {
-          title: "Title Two",
-          description:
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
-          buttonLink: "#",
-          buttonText: "View Material"
-        },
-        {
-          title: "Title Three",
-          description:
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
-          buttonLink: "#",
-          buttonText: "View Material"
-        }
-      ],
-      [
-        {
-          title: "Title Four",
-          description:
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
-          buttonLink: "#",
-          buttonText: "View Material"
-        },
-        {
-          title: "Title Five",
-          description:
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
-          buttonLink: "#",
-          buttonText: "View Material"
-        },
-        {
-          title: "Title Six",
-          description:
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
-          buttonLink: "#",
-          buttonText: "View Material"
-        }
-      ],
-      [
-        {
-          title: "Title Seven",
-          description:
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
-          buttonLink: "#",
-          buttonText: "View Material"
-        },
-        {
-          title: "Title Eight",
-          description:
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
-          buttonLink: "#",
-          buttonText: "View Material"
-        },
-        {
-          title: "Title Nine",
-          description:
-            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
-          buttonLink: "#",
-          buttonText: "View Material"
-        }
-      ]
+      {
+        title: "Title One",
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
+        buttonLink: "#",
+        buttonText: "View Material"
+      },
+      {
+        title: "Title Two",
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
+        buttonLink: "#",
+        buttonText: "View Material"
+      },
+      {
+        title: "Title Three",
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
+        buttonLink: "#",
+        buttonText: "View Material"
+      },
+
+      {
+        title: "Title Four",
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
+        buttonLink: "#",
+        buttonText: "View Material"
+      },
+      {
+        title: "Title Five",
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
+        buttonLink: "#",
+        buttonText: "View Material"
+      },
+      {
+        title: "Title Six",
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
+        buttonLink: "#",
+        buttonText: "View Material"
+      },
+
+      {
+        title: "Title Seven",
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
+        buttonLink: "#",
+        buttonText: "View Material"
+      },
+      {
+        title: "Title Eight",
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget",
+        buttonLink: "#",
+        buttonText: "View Material"
+      }
     ]
   };
   render() {
@@ -101,33 +89,7 @@ class App extends Component {
           leftLinks={leftLinks}
           rightLinks={rightLinks}
         ></NavBar>
-        <CardDeck className="cards">
-          {this.state.cards.map(cardRow => (
-            <React.Fragment>
-              <MyCard
-                buttonText={cardRow[0].buttonText}
-                title={cardRow[0].title}
-                description={cardRow[0].description}
-                buttonLink={cardRow[0].buttonLink}
-                className="card-obj"
-              />
-              <MyCard
-                buttonText={cardRow[1].buttonText}
-                title={cardRow[1].title}
-                description={cardRow[1].description}
-                buttonLink={cardRow[1].buttonLink}
-                className="card-obj"
-              />
-              <MyCard
-                buttonText={cardRow[2].buttonText}
-                title={cardRow[2].title}
-                description={cardRow[2].description}
-                buttonLink={cardRow[2].buttonLink}
-                className="card-obj"
-              />
-            </React.Fragment>
-          ))}
-        </CardDeck>
+        <CardCollection cardList={this.state.cards}></CardCollection>
       </div>
     );
   }
---
+++ src/components/common/cardCollection/cardCollection.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import MyCard from "../myCard/myCard";
+import "./cardCollection.sass";
+
+const CardCollection = ({ cardList, className, ...rest }) => {
+  return (
+    <div className={className ? className + " cards" : "cards"}>
+      {cardList.map(cardRow => (
+        <MyCard
+          buttonText={cardRow.buttonText}
+          title={cardRow.title}
+          description={cardRow.description}
+          buttonLink={cardRow.buttonLink}
+          className="card-obj"
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CardCollection;
