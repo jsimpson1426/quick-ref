6a6a09ab8598fd31d54c17c2ace2cfb959c718e6
Simpson <joshuasimpson3@usicllc.com>
2021-12-14 12:55:36 -0500

    Fixed responsive navbar; updated navbar toggle to trigger when clicking off of navbar i.e. background

--- src/components/common/navBar/navBar.jsx
+++ src/components/common/navBar/navBar.jsx
@@ -3,37 +3,59 @@ import "./navBar.sass";
 import { Link } from "react-router-dom";
 import { getCurrentUser } from "../../../services/api/auth";
 
-const NavBar = ({ navToggle, onNavToggle, onRouteChange, ...rest }) => {
-  return (
-    <nav className="topNav">
-      <Link className="leftLink" onClick={onRouteChange} to="/">
-        Quick Ref
-      </Link>
-      <div className="navToggle" onClick={onNavToggle}>
-        <div className="hamburger">
-          <div className="cylinder"></div>
-          <div className="cylinder"></div>
-          <div className="cylinder"></div>
-        </div>
-      </div>
-      <div className={navToggle ? "rightLinks-menu" : "rightLinks-menu-closed"}></div>
-      <div className={navToggle ? "rightLinks" : "rightLinks-closed"} id="navbarText">
-        <ul className="navList">
-          <li >
-            <Link to="/" onClick={onRouteChange}>
-              Home
-            </Link>
-          </li>
-          <li >
-            {getCurrentUser() && getCurrentUser().isAdmin && <Link to="/addResource" onClick={onRouteChange}>Add Resource</Link>}
-          </li>
-          <li >
-            {getCurrentUser() ? <Link  to="/logout">Logout</Link> : <Link  to="/login">Login</Link>}
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+class NavBar extends React.Component {
 
+  state = {
+    navToggle: false
+  };
+  
+  onRouteChange = () => {
+    this.setState({navToggle: false}); 
+  }
+
+  onNavToggle = () => {
+    if(!this.state.navToggle){
+      this.setState({navToggle: true});
+    } else {
+      this.setState({navToggle: false});
+    }
+  }
+
+  render() { 
+
+    return (
+      <nav className="topNav">
+        <Link className="leftLink" onClick={this.onRouteChange} to="/">
+          Quick Ref
+        </Link>
+        <div className="navToggle" onClick={this.onNavToggle}>
+          <div className="hamburger">
+            <div className="cylinder"></div>
+            <div className="cylinder"></div>
+            <div className="cylinder"></div>
+          </div>
+        </div>
+        <div className={this.state.navToggle ? "rightLinks-menu" : "rightLinks-menu-closed"} onClick={this.onNavToggle}></div>
+        <div className={this.state.navToggle ? "rightLinks" : "rightLinks-closed"} id="navbarText">
+          <ul className={this.state.navToggle ? "navList" : "navList-closed"}>
+            <li >
+              <Link to="/" onClick={this.onRouteChange}>
+                Home
+              </Link>
+            </li>
+            
+              {getCurrentUser() && getCurrentUser().isAdmin && <li ><Link to="/addResource" onClick={this.onRouteChange}>Add Resource</Link></li>}
+            
+            <li >
+              {getCurrentUser() ? <Link  to="/logout">Logout</Link> : <Link to="/login">Login</Link>}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+ 
 export default NavBar;
+
+
